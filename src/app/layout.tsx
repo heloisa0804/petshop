84612a6aaad58b1aaf934fe6974f316aaa6c7cbc
heloisa0804 <heloisa.germanobussi@yahoo.com.br>
2025-07-30 09:28:50 -0300

    Configura varíaveis para as fontes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,22 @@
 import type { Metadata } from "next";
+import { Roboto, Fjalla_One } from "next/font/google";
 import "./globals.css";
+import { wrap } from "module";
+
+// Configura varíaveis para as fontes
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-roboto",
+  display: "swap",
+});
+
+const fjallaOne = Fjalla_One({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-fjalla",
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "PetShop",
