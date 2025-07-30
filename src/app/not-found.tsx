030c2a0f89e5c0af8a514d5c009579377fd66eb0
heloisa0804 <heloisa.germanobussi@yahoo.com.br>
2025-07-30 11:26:51 -0300

    Update not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,16 @@
 //src//app/not-found.tsx
+
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Página não encontrada | PetShop ",
+  robots: "noindex",
+};
+
 export default function Pagina404() {
   return (
     <section>
-      <h2>Ops!Página não encontrada 🐶😹</h2>
+      <h2>Ops!Página não encontrada!! 🐶😹</h2>
     </section>
   );
 }
