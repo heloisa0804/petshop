f808e62e808b783e70be720b767f6351d6371ac7
heloisa0804 <heloisa.germanobussi@yahoo.com.br>
2025-07-31 11:55:47 -0300

    Adicona container nas outras paginas

--- src/app/contato/page.tsx
+++ src/app/contato/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from "next";
 import estilos from "./contato.module.css";
+import Container from "@/components/Container";
 //src//app/not-found.tsx
 export const metadata: Metadata = {
   title: "Contato | PetShop ",
@@ -9,12 +10,13 @@ export default function page() {
   return (
     <section className={estilos.conteudo}>
       <h2>Contato</h2>
-      <p>
-        Você pode nos enviar um e-mail para{" "}
-        <a href="mailto:contato@petshop.com">contato@petshop.com</a> ou nos
-        ligar pelo telefone <a href="tel:+5511999999999">+55 11 99999-9999</a>
-      </p>
-
+      <Container>
+        <p>
+          Você pode nos enviar um e-mail para{" "}
+          <a href="mailto:contato@petshop.com">contato@petshop.com</a> ou nos
+          ligar pelo telefone <a href="tel:+5511999999999">+55 11 99999-9999</a>
+        </p>
+      </Container>
       <p>Se preferir, use o formulário abaixo:</p>
     </section>
   );
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,6 +3,7 @@
 import { Metadata } from "next";
 import estilos from "./not-found.module.css";
 import Link from "next/link";
+import Container from "@/components/Container";
 export const metadata: Metadata = {
   title: "Página não encontrada | PetShop ",
   robots: "noindex",
@@ -12,15 +13,17 @@ export default function Pagina404() {
   return (
     <section className={estilos.conteudo}>
       <h2>Ops!Página não encontrada!!</h2>
-      <div className={estilos.imagem404}>
-        <img
-          src="./images/404.svg"
-          alt="Cachorrinho com cara triste e nariz vermelho com símbolo de interrogação"
-        />
-        <p>
-          <Link href="/"> Voltar para a página inicial</Link>
-        </p>
-      </div>
+      <Container>
+        <div className={estilos.imagem404}>
+          <img
+            src="./images/404.svg"
+            alt="Cachorrinho com cara triste e nariz vermelho com símbolo de interrogação"
+          />
+          <p>
+            <Link href="/"> Voltar para a página inicial</Link>
+          </p>
+        </div>
+      </Container>
     </section>
   );
 }
