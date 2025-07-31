da14a8ccf6b0f291c20618059cc2736cfe967e42
heloisa0804 <heloisa.germanobussi@yahoo.com.br>
2025-07-31 09:51:55 -0300

    Deixa o menu ativo

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,13 +1,37 @@
 //src/components/menu.tsx
+
+"use client";
+
 import Link from "next/link";
+import estilos from "./Menu.module.css";
+import { usePathname } from "next/navigation";
 
 export default function Menu() {
+  const pathname = usePathname();
+
   return (
-    <nav>
-      <Link href="/">Blog</Link>
-      <Link href="/produtos">Produtos</Link>
-      <Link href="/sobre">Sobre</Link>
-      <Link href="/contato">Contato</Link>
+    <nav className={estilos.menu}>
+      <Link href="/" className={pathname === "/" ? estilos.ativo : ""}>
+        Blog
+      </Link>
+      <Link
+        href="/produtos"
+        className={pathname === "/produtos" ? estilos.ativo : ""}
+      >
+        Produtos
+      </Link>
+      <Link
+        href="/sobre"
+        className={pathname === "/sobre" ? estilos.ativo : ""}
+      >
+        Sobre
+      </Link>
+      <Link
+        href="/contato"
+        className={pathname === "/contato" ? estilos.ativo : ""}
+      >
+        Contato
+      </Link>
     </nav>
   );
 }
