ca0ef78488eb66224c7f89d3d9434671943d0ab9
heloisa0804 <heloisa.germanobussi@yahoo.com.br>
2025-08-01 09:50:07 -0300

    Configura props para o Listaposts

--- src/components/ListasPosts.tsx
+++ src/components/ListasPosts.tsx
@@ -1,7 +1,13 @@
 //src/components/ListaPosts.txs
+import { Post } from "@/types/Post";
 import estilos from "./ListaPosts.module.css";
 
-export default function ListasPosts() {
+// Configurando um tipo para este componente e, neste tipo, definimos a prop post relacionado ela ao tipo Post criado anteriormente.
+type ListaPostProps = {
+  post: Post[];
+};
+
+export default function ListasPosts({ post }: ListaPostProps) {
   return (
     <div className={estilos.posts}>
       <article>
